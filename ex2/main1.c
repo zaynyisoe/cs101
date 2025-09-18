686462bb4cd9d25e2457137e3b5f6e2e2df51a8a
zaynyisoe <zaynyisoe909@gmail.com>
2025-09-18 15:48:16 +0800

    Update main1.c

--- ex2/main1.c
+++ ex2/main1.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+int main() {
+ char a = 5;
 
+ printf("\t%d\n", a&1);
+ // 0101 & 0001 = 0001
+
+ printf("\t%d\n", a&2);
+ // 0101 & 0010 = 0000
+
+ printf("\t%d\n", al2);
+ // 0101 | 0010 = 0111
+ return 0;
+
+}
