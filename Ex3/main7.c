ad50c77d4597dbc6909986ba2f00d439c3c99a0d
zaynyisoe <zaynyisoe909@gmail.com>
2025-10-02 14:29:07 +0800

    Create main7.c

---
+++ Ex3/main7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int i = 1599;
+    if (i <= 1500) {
+        printf("70元");
+    }
+    else {
+        int n = i - 1500;
+        if (n%100) {
+            int h= ((n/100)+1) * 10;
+            printf("%d 元",70+h);
+        }
+        else {
+            printf("%d 元",70+(n/100)*10);
+        }
+    }
+    return 0;
+}
