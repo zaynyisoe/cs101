a69e93d634134a9fa6f502920ecf6e3834cd2b0c
zaynyisoe <zaynyisoe909@gmail.com>
2025-10-23 15:15:42 +0800

    Delete Ex3/main35.c

--- Ex3/main35.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char v = 65;       // 'A'
-    int *p = (int *)&v; // Wrong pointer type
-    printf("%d\n", *p); // ❌ reading 4 bytes from 1-byte variable
-    return 0;
-}
