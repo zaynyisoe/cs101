d987682292f52d85539705d93c9a5d5bd1c0269b
zaynyisoe <zaynyisoe909@gmail.com>
2025-12-11 14:41:48 +0800

    Create ReadClass.cpp

---
+++ Ex9/ReadClass.cpp
@@ -0,0 +1,38 @@
+#include "ReadClass.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+ReadClass::ReadClass(const std::string &fname) : filename(fname) {}
+
+void ReadClass::process() {
+    std::ifstream fin(filename);
+    if (!fin) {
+        std::cout << "Cannot open file: " << filename << std::endl;
+        return;
+    }
+
+    std::string word;
+    while (fin >> word) {
+        if (word == "class") {
+            std::string className;
+            fin >> className;
+
+            // remove { or : if attached
+            while (!className.empty() &&
+                  (className.back() == '{' || className.back() == ':'))
+            {
+                className.pop_back();
+            }
+
+            classNames.push_back(className);
+        }
+    }
+}
+
+void ReadClass::printResult() const {
+    std::cout << "Found " << classNames.size() << " class(es):\n";
+    for (const auto &c : classNames) {
+        std::cout << c << "\n";
+    }
+}
